67bacda163b4bf61ca641c8a46a4e598c1e5b531
Abdulrhman11AA <113008816+Abdulrhman11AA@users.noreply.github.com>
2022-10-01 08:09:53 +0200

    Add files via upload
    Polycarp urgently needs a shovel! He comes to the shop and chooses an appropriate one. The shovel that Policarp chooses is sold for k burles. Assume that there is an unlimited number of such shovels in the shop.
    
    In his pocket Polycarp has an unlimited number of "10-burle coins" and exactly one coin of r burles (1 ≤ r ≤ 9).
    
    What is the minimum number of shovels Polycarp has to buy so that he can pay for the purchase without any change? It is obvious that he can pay for 10 shovels without any change (by paying the requied amount of 10-burle coins and not using the coin of r burles). But perhaps he can buy fewer shovels and pay without any change. Note that Polycarp should buy at least one shovel.
    
    Input
    The single line of input contains two integers k and r (1 ≤ k ≤ 1000, 1 ≤ r ≤ 9) — the price of one shovel and the denomination of the coin in Polycarp's pocket that is different from "10-burle coins".
    
    Remember that he has an unlimited number of coins in the denomination of 10, that is, Polycarp has enough money to buy any number of shovels.
    
    Output
    Print the required minimum number of shovels Polycarp has to buy so that he can pay for them without any change.

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int price = 0, coin = 0,counter =0 ,result=0;
+	cin >> price >> coin;
+	while (true) {
+		counter++;
+		result = price * counter;
+		if (result % 10 == 0 || result % 10 == coin) break;
+		cout << "proccicng\n";
+	}
+	cout << counter;
+	return 0;
+}
+
+/*
+int main() {
+    // An oher way 
+	int price = 0, coin = 0; int ten = 10,counter =1;
+	cin >> price >> coin;
+	int PRICE = price;
+	while (true) {
+		if (ten + coin == price || ten == price || coin ==price) {
+			break;
+		}
+		else if (ten + coin > price) {
+			price += PRICE;
+			counter++;
+		} else
+			ten +=10;
+	}
+	cout << counter << endl;
+	return 0;
+}
+*/
